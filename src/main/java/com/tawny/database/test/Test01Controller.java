12c3324d15e372ea5ac97b1aad2cc57abb06fb02
DESKTOP-5I9A261\tawny <tawny6308@naver.com>
2023-07-01 05:51:13 +0900

    (원영도) .database 연동 test01 (출력 값 변수 저장)

--- src/main/java/com/tawny/database/test/Test01Controller.java
+++ src/main/java/com/tawny/database/test/Test01Controller.java
@@ -3,13 +3,16 @@ package com.tawny.database.test;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.tawny.common.MysqlService;
 
+@WebServlet("/db/test01")
 public class Test01Controller extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -24,15 +27,28 @@ public class Test01Controller extends HttpServlet {
 		String selectQuery = "SELECT * FROM `real_estate`";
 		ResultSet resultSet = mysqlService.select(selectQuery);
 		
-		while(resultSet.next()) {
-			resultSet.getInt("realtorld");
-			resultSet.getString("");
-			resultSet.getInt("");
-			resultSet.getString("");
-			resultSet.getInt("");
-			resultSet.getInt("");
-		}
-		
+		out.println("<html><head><title>매물리스트</title></head><body>");
+			try {
+				while(resultSet.next()) {
+					int realtorld = resultSet.getInt("realtorld");
+					String address = resultSet.getString("address");
+					int area = resultSet.getInt("area");
+					String type = resultSet.getString("type");
+					int price = resultSet.getInt("price");
+					int renPrice = resultSet.getInt("renPrice");
+					
+					// if() {
+						out.println("<div>매물주소 : " + address + ", 면적 : " + area + ", 타입 : " + type + "</div>");
+						
+					// }
+				}
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+		out.println("</body></html>");		
+	
+			
 		
 	}
 }
